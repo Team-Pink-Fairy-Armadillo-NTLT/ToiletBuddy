086f0784a3ed0bd36c601ed1ee3aaa8e18d5fb18
Tdjergian <tdjerg@gmail.com>
2024-04-13 13:42:33 -0700

    merge conflict change

--- client/components/Bathroom.jsx
+++ client/components/Bathroom.jsx
@@ -53,11 +53,7 @@ const Bathroom = ()=>{
 
   return(
     <section id='bathroomSect'>
-<<<<<<< HEAD
-      <h1>{placeName}</h1>
-=======
       {placeName}
->>>>>>> dev
       <br></br>
       <input id='review' placeholder='Add a review'></input> <button onClick={()=>{addReview(placeId)}}> Submit review</button>
       <div id='bathroomReviews'>
