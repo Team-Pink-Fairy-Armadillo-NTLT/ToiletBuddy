8cbea90278b0e9288508d823954c44d103a8b87f
Nicolette Sookar <nicolettesookar@gmail.com>
2024-04-19 00:53:10 -0400

    make place id in route optional

--- server/routes/googleAuthRouter.js
+++ server/routes/googleAuthRouter.js
@@ -9,9 +9,13 @@ const router = express.Router();
 
 const { CLIENT_ID, CLIENT_SECRET } = process.env;
 
+// const baseUrl = webpack.mode == "production" ? "https://tsa2snumdz.us-east-2.awsapprunner.com" : 'http://localhost:8080'
+// const callbackURL = baseUrl + "/google/callback"
+
 const callbackURL = webpack.mode === 'production'
 ? 'http://localhost:3000/google/callback'
 : 'http://localhost:8080/google/callback';
+
 
 const oauth2Client = new google.auth.OAuth2(
   CLIENT_ID,
@@ -21,7 +25,7 @@ const oauth2Client = new google.auth.OAuth2(
 
 const GOOGLE_OAUTH_URL = "https://www.googleapis.com/oauth2/v2/userinfo"
 
-router.get('/auth/:placeId', (req, res) => {
+router.get('/auth/:placeId?', (req, res) => {
   // console.log('in the auth')
   const placeId = req.params.placeId;
   const scopes = [
